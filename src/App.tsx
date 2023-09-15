e6f5cb47aa10055cbb26f49f14a9d7ef2fb16e82
Vadim Bochkarev <frogix@ya.ru>
2023-09-15 21:18:44 +0500

    feat: added theme config, updated default elements with antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { Select, Spin } from "antd";
+import { Layout, Menu, Select, Spin, theme } from "antd";
 import { GameInfo, getGamesList } from "./api/games";
 import { ListOfGames } from "./ListOfGames";
+import Title from "antd/es/typography/Title";
+import Text from "antd/es/typography/Text";
+import { Content, Footer, Header } from "antd/es/layout/layout";
 
 function App() {
 	const [isLoading, setLoading] = useState(true);
@@ -21,21 +24,20 @@ function App() {
 		const uniqueGenres = new Set(allGenres);
 		setGenres(Array.from(uniqueGenres));
 
-		setGenreOptions(Array.from(uniqueGenres).map(g => ({label: g, value: g}) ));
+		setGenreOptions(Array.from(uniqueGenres).map((g) => ({ label: g, value: g })));
 	};
 
 	const handleGenreChanged = (genres: string[]) => {
 		setGenreFilter(genres);
-	}
+	};
 
 	const filterGame = (game: GameInfo) => {
-		if (genreFilter.length > 0)
-		{
-			return genreFilter.includes(game.genre)
+		if (genreFilter.length > 0) {
+			return genreFilter.includes(game.genre);
 		}
 
 		return true;
-	}
+	};
 
 	useEffect(() => {
 		getGamesList()
@@ -46,22 +48,36 @@ function App() {
 			.finally(() => setLoading(false));
 	}, []);
 
+	const {
+		token: { colorBgContainer }
+	} = theme.useToken();
+
 	return (
 		<>
-			<h1>Free to Game</h1>
+			<Layout>
+				<Header>
+					<Menu theme="dark" mode="horizontal" items={[{ key: 1, label: "Main!" }]} />
+				</Header>
+			</Layout>
+			<Layout>
+				<Content style={{ padding: "0 50px", backgroundColor: colorBgContainer }}>
+					<Title level={1}>Free to Game</Title>
 
-			<Select
-				mode="multiple"
-				allowClear
-				style={{ width: '100%' }}
-				placeholder="Select genres"
-				defaultValue={[]}
-				onChange={handleGenreChanged}
-				options={genreOptions}
-			/>
-			{isLoading && <Spin spinning={isLoading} />}
-			{error && "A network error has occured. Please try again later."}
-			{games.length > 0 && <ListOfGames games={games.filter(filterGame)} />}
+					<Select
+						mode="multiple"
+						allowClear
+						style={{ width: "100%" }}
+						placeholder="Select genres"
+						defaultValue={[]}
+						onChange={handleGenreChanged}
+						options={genreOptions}
+					/>
+					{isLoading && <Spin spinning={isLoading} />}
+					{error && "A network error has occured. Please try again later."}
+					{games.length > 0 && <ListOfGames games={games.filter(filterGame)} />}
+				</Content>
+			<Footer style={{textAlign: "center"}}>Created by frogix in 2023.</Footer>
+			</Layout>
 		</>
 	);
 }
--- src/GameCard.tsx
+++ src/GameCard.tsx
@@ -1,5 +1,6 @@
-import { Descriptions, DescriptionsProps } from "antd";
+import { Descriptions, DescriptionsProps, Image } from "antd";
 import { GameInfo } from "./api/games";
+import Title from "antd/es/typography/Title";
 
 function gameInfoToDescriptionItems(info: GameInfo) {
 	const items: DescriptionsProps["items"] = [];
@@ -8,7 +9,7 @@ function gameInfoToDescriptionItems(info: GameInfo) {
 		let value = info[key];
 
 		if (key === "thumbnail") {
-			value = <img src={value} />;
+			value = <Image src={value} />;
 		}
 		items.push({
 			key,
@@ -24,7 +25,7 @@ export function GameCard(props: GameInfo) {
 
     return (
         <>
-            <h2>{props.title}</h2>
+            <Title level={2}>{props.title}</Title>
             <Descriptions column={1} items={gameInfoToDescriptionItems(props)}></Descriptions>
         </>
     );
