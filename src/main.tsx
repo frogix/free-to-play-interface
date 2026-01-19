a4747704851017b5f6b559b40a95df0c872bc2fb
Vadim Bochkarev <frogix@ya.ru>
2026-01-19 16:39:28 +0300

    fix(deploy): use gh-pages basename only in prod

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
 
 import "./index.css";
-import { Root } from "./routes/root.tsx";
-import { GameCardPage } from "./routes/game.tsx";
-import { GamesListScreen } from "./components/GamesListScreen.tsx";
+import {Root} from "./routes/root.tsx";
+import {GameCardPage} from "./routes/game.tsx";
+import {GamesListScreen} from "./components/GamesListScreen.tsx";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <Root />,
+			children: [
+				{
+					index: true,
+					element: <GamesListScreen />
+				},
+				{
+					path: "game/:gameId",
+					element: <GameCardPage />,
+					loader: GameCardPage.loader
+				}
+			]
+		}
+	],
 	{
-		path: "/",
-		element: <Root />,
-		children: [
-			{
-				index: true,
-				element: <GamesListScreen />
-			},
-			{
-				path: "game/:gameId",
-				element: <GameCardPage />,
-				loader: GameCardPage.loader
-			}
-		]
+		basename: import.meta.env.BASE_URL
 	}
-]);
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	base: "/free-to-play-interface/",
+export default defineConfig(({mode}) => {
+	return {
+	base: mode === "production" ? "/free-to-play-interface/" : "",
 	plugins: [react()],
+	}
 })
