727167e3df561b912f9ca3ecfc863aafd2a8d2c5
Vadim Bochkarev <frogix@ya.ru>
2026-01-19 13:26:52 +0300

    fix(deploy): added deploy workflow, fix base dir for vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "./", // use relative path
+	base: "/free-to-play-interface/",
 	plugins: [react()],
 })
