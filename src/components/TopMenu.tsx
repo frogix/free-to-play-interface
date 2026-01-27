e67e3f40440594e67fa1032f2b437d7bbd5f24ad
Vadim Bochkarev <frogix@ya.ru>
2026-01-27 21:17:44 +0300

    style: avoid caption shifting when back button appears

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -2,28 +2,35 @@ import { LeftOutlined } from "@ant-design/icons";
 import Menu from "antd/es/menu";
 import { Link, useLocation } from "react-router-dom";
 
+
 export function TopMenu() {
 	const location = useLocation();
 
 	const items = [
 		{
 			key: "1",
-			label: <Link to="/">Free to play games</Link>,
+			label:
+				<Link
+					to="/"
+					style={{
+						position: "relative",
+					}}
+				>
+					{
+						location.pathname !== "/" &&
+						<LeftOutlined
+							style={{
+								position: "absolute",
+								left: 0,
+								top: "50%",
+								transform: "translate(-20px, -50%)",
+							}}
+						/>
+					}
+					Free to play games
+				</Link>
 		}
 	]
-
-	if (location.pathname !== "/") {
-		items.unshift(
-			{
-				key: "0",
-				label: (
-					<Link to="/">
-						<LeftOutlined />
-					</Link>
-				),
-			},
-		);
-	}
 
 	return (
 		<Menu selectable={false} theme="dark" mode="horizontal" items={items} />
